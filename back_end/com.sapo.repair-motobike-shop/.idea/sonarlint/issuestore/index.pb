ca25a5de8b682cfc1f6f8a44a6a6d5c54c825c9a
datbt3 <killer10011803@gmail.com>
2021-12-12 20:39:07 +0700

    Dat commit backend

---
+++ back_end/com.sapo.repair-motobike-shop/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+p
+@src/main/java/com/sapo/services/impl/UserDetailsServiceImpl.java,d\0\d0e44f7deb9deb6013ef92b55663a990ad71e17a
+i
+9src/main/java/com/sapo/services/impl/UserServiceImpl.java,3\a\3a99e814d9898cf958b92a8a1d2254f7f42ab0d1
+t
+Dsrc/main/java/com/sapo/services/impl/VehicleCustomerServiceImpl.java,d\7\d7d4706b41c8c421b2511a3a87c30a59688c9c9b
---
+++ back_end/com.sapo.repair-motobike-shop/src/main/java/com/sapo/StartWebApplication.java
@@ -0,0 +1,18 @@
+package com.sapo;
+
+import com.sapo.common.Common;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Random;
+
+
+@SpringBootApplication
+public class StartWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(StartWebApplication.class, args);
+        System.out.println(Common.GeneratePassword("1234567890"));
+    }
+}
